a4584ffaa36f4bea3541bd07a7a765c29b7165e0
Suyash Gardi <suyashgardi7119@gmail.com>
2026-04-01 23:13:39 +0530

    Refactor useAuth to include API base URL
    Updated API endpoint to use a base URL and added withCredentials option for axios request.

--- src/CustomHooks/userAuth.js
+++ src/CustomHooks/userAuth.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import API from '../../api.js';
 
 export function useAuth() {
   const [user, setUser] = useState(null);
- 
-  const [isLoading, setIsLoading] = useState(true); 
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     const checkSession = async () => {
       try {
-        const response = await axios.get("/api/me");
+        const response = await axios.get(`${API}/api/me`, { withCredentials: true });
         if (response.data.loggedIn) {
           setUser(response.data.user);
         } else {
@@ -20,11 +20,9 @@ export function useAuth() {
       } catch (err) {
         navigate("/login");
       } finally {
-
-        setIsLoading(false); 
+        setIsLoading(false);
       }
     };
-    
     checkSession();
   }, [navigate]);
 
