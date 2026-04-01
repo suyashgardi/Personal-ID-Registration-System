ef6eb7abf1cc330ccd71a56d703a07d92d83d776
Suyash Gardi <suyashgardi7119@gmail.com>
2026-04-01 23:11:38 +0530

    Update getUsers.js

--- src/CustomHooks/getUsers.js
+++ src/CustomHooks/getUsers.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import API from '../../api.js';
 
 export function GetUsers() {
   const [idCards, setIdCards] = useState([]);
@@ -7,16 +8,14 @@ export function GetUsers() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("/api/userids");
-        if (response && response.data) {
-          setIdCards(response.data);
-        }
+        const response = await axios.get(`${API}/api/userids`, { withCredentials: true });
+        if (response && response.data) setIdCards(response.data);
       } catch (error) {
         console.error("Error fetching user IDs:", error);
       }
     };
-
     fetchUsers();
-  }, []); 
-  return [idCards, setIdCards]; 
+  }, []);
+
+  return [idCards, setIdCards];
 }
