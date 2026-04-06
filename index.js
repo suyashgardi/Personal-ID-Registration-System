76ed641434645cca6f1fd665a977658b84c58baa
Suyash Gardi <suyashgardi7119@gmail.com>
2026-04-06 13:22:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const port = 5000;
 import dotenv from "dotenv";
 dotenv.config();
 
+// app.use(cors({
+//   origin:   process.env.FRONTEND_URL || "http://localhost:5173" ,
+//   credentials: true
+// }));
 app.use(cors({
-  origin:  "https://secure-virtual-id-system-with-biome.vercel.app" || process.env.FRONTEND_URL || "http://localhost:5173" ,
-  credentials: true
+  origin: "*",
+  methods: "*",
+  allowedHeaders: "*"
 }));
 
 // app.use(cors());
